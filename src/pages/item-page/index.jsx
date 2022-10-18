e192a06b03dada87e7205be9a079057a633b73b2
zilvinas-dvareckas <95238038+zdvareckas@users.noreply.github.com>
2022-10-18 20:10:57 +0300

    feat: single bike display page init

--- src/pages/item-page/index.jsx
+++ src/pages/item-page/index.jsx
@@ -1,3 +1,4 @@
+import { Box, Button, Typography } from '@mui/material';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { bikeService } from '../../services/bikes-service';
@@ -9,13 +10,83 @@ const ItemPage = () => {
   React.useEffect(() => {
     (async () => {
       const data = await bikeService.fetchById(itemId);
-      setItem(data);
       console.log(data);
+      setItem(data);
     })();
   }, [itemId]);
 
   return (
-    <pre>{JSON.stringify(item, null, 4)}</pre>
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+
+      <Box sx={{
+        display: 'flex',
+        gap: 5,
+        justifyContent: 'center',
+        p: 5,
+        width: '100%',
+        flexDirection: 'row',
+      }}
+      >
+
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 1,
+          width: '60%',
+          // height: 300,
+          maxHeight: 700,
+          overflow: 'auto',
+          '&::-webkit-scrollbar': { width: 0 },
+        }}
+        >
+          <Box
+            component="img"
+            src={item.img}
+          />
+          <Box
+            component="img"
+            src={item.img}
+          />
+          <Box
+            component="img"
+            src={item.img}
+          />
+
+        </Box>
+
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'start',
+          width: { xs: '100%', md: '40%' },
+          gap: 2,
+        }}
+        >
+
+          <Typography variant="h2">
+            {item.title}
+          </Typography>
+
+          <Typography variant="h5">{item.price}</Typography>
+
+          <Typography variant="h6">Select size:</Typography>
+          <Box sx={{ display: 'flex', gap: 1 }}>
+            <Button variant="outlined">{item.size?.label}</Button>
+          </Box>
+
+          <Typography
+            variant="body2"
+            sx={{ width: '50%' }}
+          >
+            {item.description}
+
+          </Typography>
+
+        </Box>
+
+      </Box>
+    </Box>
+
   );
 };
 
