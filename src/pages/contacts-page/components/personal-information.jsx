b3b4d98f9930125678a014f14710a667565c29c5
zilvinas-dvareckas <95238038+zdvareckas@users.noreply.github.com>
2022-10-20 18:47:45 +0300

    refactor: placed yup validation in main contacts page

--- src/pages/contacts-page/components/personal-information.jsx
+++ src/pages/contacts-page/components/personal-information.jsx
@@ -5,123 +5,92 @@ import {
   Divider,
   TextField,
 } from '@mui/material';
-import * as yup from 'yup';
-import { useFormik } from 'formik';
 
-const initialValues = {
-  firstName: '',
-  surname: '',
-  email: '',
-  phoneNumber: null,
-  question: '',
-};
-
-const validationSchema = yup.object({
-  firstName: yup.string()
-    .required('Name is required'),
-  surname: yup.string()
-    .required('Surname is required'),
-  email: yup.string()
-    .required('Email is required'),
-  phoneNumber: yup.string(),
-  question: yup.string()
-    .required('Question is required'),
-});
-
-const PersonalInformation = () => {
-  const onSubmit = (values) => {
-    console.log('įvestos reikšmės');
-    console.table(values);
-  };
-
-  const {
-    values, errors, touched, dirty, isValid,
-    handleChange, handleBlur, handleSubmit,
-  } = useFormik({
-    initialValues,
-    validationSchema,
-    onSubmit,
-  });
-
-  return (
-    <>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: { xs: 'column', lg: 'row' },
-          gap: 1,
-        }}
-        component="form"
-        onSubmit={handleSubmit}
-      >
-        <TextField
-          fullWidth
-          label="Name"
-          variant="filled"
-          name="firstName"
-          value={values.firstName}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          error={touched.firstName && Boolean(errors.firstName)}
-          helperText={touched.firstName && errors.firstName}
-        />
-        <TextField
-          fullWidth
-          label="Surname"
-          variant="filled"
-          name="surname"
-          value={values.surname}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          error={touched.surname && Boolean(errors.surname)}
-          helperText={touched.surname && errors.surname}
-        />
-      </Box>
+const PersonalInformation = ({
+  values,
+  errors,
+  touched,
+  dirty,
+  isValid,
+  handleChange,
+  handleBlur,
+}) => (
+  <>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: { xs: 'column', lg: 'row' },
+        gap: 1,
+      }}
+    >
       <TextField
         fullWidth
-        label="E-mail"
+        label="Name"
         variant="filled"
-        name="email"
-        value={values.email}
+        name="firstName"
+        value={values.firstName}
         onChange={handleChange}
         onBlur={handleBlur}
-        error={touched.email && Boolean(errors.email)}
-        helperText={touched.email && errors.email}
+        error={touched.firstName && Boolean(errors.firstName)}
+        helperText={touched.firstName && errors.firstName}
       />
       <TextField
         fullWidth
-        label="Phone number"
+        label="Surname"
         variant="filled"
-        name="phone"
-        value={values.phoneNumber}
+        name="surname"
+        value={values.surname}
         onChange={handleChange}
         onBlur={handleBlur}
-        error={touched.phoneNumber && Boolean(errors.phoneNumber)}
-        helperText={touched.phoneNumber && errors.phoneNumber}
+        error={touched.surname && Boolean(errors.surname)}
+        helperText={touched.surname && errors.surname}
       />
+    </Box>
+    <TextField
+      fullWidth
+      label="E-mail"
+      variant="filled"
+      name="email"
+      value={values.email}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      error={touched.email && Boolean(errors.email)}
+      helperText={touched.email && errors.email}
+    />
+    <TextField
+      fullWidth
+      label="Phone number"
+      variant="filled"
+      name="phoneNumber"
+      type="number"
+      value={values.phoneNumber}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      error={touched.phoneNumber && Boolean(errors.phoneNumber)}
+      helperText={touched.phoneNumber && errors.phoneNumber}
+    />
 
-      <TextField
-        fullWidth
-        label="Question"
-        variant="filled"
-        name="question"
-        value={values.question}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        error={touched.question && Boolean(errors.question)}
-        helperText={touched.question && errors.question}
-      />
-      <Button
-        type="submit"
-        variant="contained"
-        color="success"
-        disabled={!dirty || !isValid}
-      >
-        Contact..
-      </Button>
-      <Divider />
-    </>
-  );
-};
+    <TextField
+      fullWidth
+      label="Question"
+      variant="filled"
+      name="question"
+      value={values.question}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      error={touched.question && Boolean(errors.question)}
+      helperText={touched.question && errors.question}
+    />
+    <Button
+      type="submit"
+      variant="contained"
+      color="success"
+      disabled={!dirty || !isValid}
+    >
+      Contact..
+    </Button>
+    <Divider />
+  </>
+);
 
 export default PersonalInformation;
--- src/pages/contacts-page/index.jsx
+++ src/pages/contacts-page/index.jsx
@@ -1,21 +1,74 @@
 import React from 'react';
 import { Box } from '@mui/material';
-import { Form } from './components';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
 import PersonalInformation from './components/personal-information';
 import Contacts from './components/contacts';
+import { Form } from './components';
 
-const ContactsPage = () => (
-  <Box sx={{
-    display: 'flex',
-    justifyContent: 'center',
-    pt: 5,
-  }}
-  >
-    <Form component="form">
-      <PersonalInformation />
-      <Contacts />
-    </Form>
-  </Box>
-);
+const initialValues = {
+  firstName: '',
+  surname: '',
+  email: '',
+  phoneNumber: '',
+  question: '',
+};
+
+const validationSchema = yup.object({
+  firstName: yup.string()
+    .required('Name is required')
+    .max(10, 'Name is too long')
+    .matches(/^[A-Za-z, Ąą, Čč, Ęę, Ėė, Įį, Šš, Ųų Ūū, Žž]+$/, 'Name must contain only letters'),
+  surname: yup.string()
+    .required('Surname is required')
+    .max(10, 'Surname is too long')
+    .matches(/^[A-Za-z, Ąą, Čč, Ęę, Ėė, Įį, Šš, Ųų Ūū, Žž]+$/, 'Name must contain only letters'),
+  email: yup.string()
+    .required('Email is required')
+    .email('Invalid email format'),
+  phoneNumber: yup.string()
+    .min(9, 'Number is too short')
+    .max(11, 'Number is too long'),
+  question: yup.string()
+    .required('Question is required')
+    .matches(/^[A-Za-z, Ąą, Čč, Ęę, Ėė, Įį, Šš, Ųų Ūū, Žž]+$/, 'Name must contain only letters'),
+});
+
+const ContactsPage = () => {
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  const {
+    values, errors, touched, dirty, isValid,
+    handleChange, handleBlur, handleSubmit,
+  } = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
+
+  return (
+    <Box sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      pt: 5,
+    }}
+    >
+      <Form component="form" onSubmit={handleSubmit}>
+        <PersonalInformation
+          values={values}
+          errors={errors}
+          touched={touched}
+          dirty={dirty}
+          isValid={isValid}
+          handleChange={handleChange}
+          handleBlur={handleBlur}
+        />
+        <Contacts />
+      </Form>
+    </Box>
+  );
+};
 
 export default ContactsPage;
