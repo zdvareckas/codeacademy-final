02a5ac633cae98fd5339fbdaa84a2223bc258f36
zilvinas-dvareckas <95238038+zdvareckas@users.noreply.github.com>
2022-10-20 17:31:59 +0300

    feat: contact form formik validation

--- src/pages/contacts-page/components/personal-information.jsx
+++ src/pages/contacts-page/components/personal-information.jsx
@@ -5,36 +5,76 @@ import {
   Divider,
   TextField,
 } from '@mui/material';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
+
+const initialValues = {
+  firstName: '',
+  surname: '',
+  email: '',
+  phoneNumber: null,
+  question: '',
+};
+
+const validationSchema = yup.object({
+  firstName: yup.string()
+    .required('Name is required'),
+  surname: yup.string()
+    .required('Surname is required'),
+  email: yup.string()
+    .required('Email is required'),
+  phoneNumber: yup.string(),
+  question: yup.string()
+    .required('Question is required'),
+});
 
 const PersonalInformation = () => {
-  const [firstName, setFirstName] = React.useState('');
-  const [lastName, setLastName] = React.useState('');
-  const [email, setEmail] = React.useState('');
-  const [phoneNumber, setPhoneNumber] = React.useState('');
+  const onSubmit = (values) => {
+    console.log('įvestos reikšmės');
+    console.table(values);
+  };
+
+  const {
+    values, errors, touched, dirty, isValid,
+    handleChange, handleBlur, handleSubmit,
+  } = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
 
   return (
     <>
-      <Box sx={{
-        display: 'flex',
-        flexDirection: { xs: 'column', lg: 'row' },
-        gap: 1,
-      }}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: { xs: 'column', lg: 'row' },
+          gap: 1,
+        }}
+        component="form"
+        onSubmit={handleSubmit}
       >
         <TextField
           fullWidth
           label="Name"
           variant="filled"
           name="firstName"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
+          value={values.firstName}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          error={touched.firstName && Boolean(errors.firstName)}
+          helperText={touched.firstName && errors.firstName}
         />
         <TextField
           fullWidth
           label="Surname"
           variant="filled"
-          name="lastName"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
+          name="surname"
+          value={values.surname}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          error={touched.surname && Boolean(errors.surname)}
+          helperText={touched.surname && errors.surname}
         />
       </Box>
       <TextField
@@ -42,19 +82,43 @@ const PersonalInformation = () => {
         label="E-mail"
         variant="filled"
         name="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        value={values.email}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={touched.email && Boolean(errors.email)}
+        helperText={touched.email && errors.email}
       />
       <TextField
         fullWidth
         label="Phone number"
         variant="filled"
         name="phone"
-        value={phoneNumber}
-        onChange={(e) => setPhoneNumber(e.target.value)}
+        value={values.phoneNumber}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={touched.phoneNumber && Boolean(errors.phoneNumber)}
+        helperText={touched.phoneNumber && errors.phoneNumber}
       />
 
-      <Button type="submit" variant="contained" color="success">Contact..</Button>
+      <TextField
+        fullWidth
+        label="Question"
+        variant="filled"
+        name="question"
+        value={values.question}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={touched.question && Boolean(errors.question)}
+        helperText={touched.question && errors.question}
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        color="success"
+        disabled={!dirty || !isValid}
+      >
+        Contact..
+      </Button>
       <Divider />
     </>
   );
