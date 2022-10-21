0b429dab1b86f98fee4d6de403aa3fa8ea148fe3
zilvinas-dvareckas <95238038+zdvareckas@users.noreply.github.com>
2022-10-21 15:15:57 +0300

    feat: login and register pages init

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -8,12 +8,18 @@ import EquipmentsPage from '../pages/equipments-page';
 import CartPage from '../pages/cart-page';
 import ContactsPage from '../pages/contacts-page';
 import EquipmentPage from '../pages/equipment-page';
+import LoginPage from '../pages/login-page';
+import RegisterPage from '../pages/register-page';
 
 const PageRoutes = () => (
   <Routes>
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="bikes" element={<BikesPage />} />
+      <Route path="auth">
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+      </Route>
       <Route path="equipment" element={<EquipmentsPage />} />
       <Route path="bike/:bikeId" element={<BikePage />} />
       <Route path="equipment/:equipmentId" element={<EquipmentPage />} />
